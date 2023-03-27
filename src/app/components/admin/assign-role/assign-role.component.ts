e2d4c7229d561d8825431a72a86e03fe329f43d5
John Doe <jghevariya@asite.com>
2023-03-27 22:59:01 +0530

    test

--- src/app/components/admin/assign-role/assign-role.component.ts
+++ src/app/components/admin/assign-role/assign-role.component.ts
@@ -1,78 +1,25 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {
-  MatSelectionList,
-  MatSelectionListChange,
-} from '@angular/material/list';
-
+const ELEMENT_DATA = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+];
 @Component({
   selector: 'app-assign-role',
   templateUrl: './assign-role.component.html',
   styleUrls: ['./assign-role.component.css'],
 })
 export class AssignRoleComponent implements OnInit {
-  hide: boolean = false;
-  privilages: {
-    privilage: string;
-  }[] = [
-    { privilage: 'Program Activation' },
-    { privilage: 'Talent Evaluation' },
-    { privilage: 'Talent Approvals' },
-    { privilage: 'Enrollment Approvals' },
-    { privilage: 'Add Program' },
-    { privilage: 'Add Trainer' },
-  ];
-  selected = {};
-  selectAll: boolean = false;
-  @ViewChild(MatSelectionList) myList!: MatSelectionList;
-  selectedOptions: string[] = [];
-  assignRoleForm!: FormGroup;
-  // toggleSelection(obj: any) {
-  //   this.selected[privilage as keyof privilages] =
-  //     !this.selected[obj.privilage];
-  // }
-  constructor() {}
+  ngOnInit() {}
 
-  // toggleSelectAll() {
-  //   this.selectAll = !this.selectAll;
-  //   if (this.selectAll) {
-  //     this.privilages.forEach((obj) => {
-  //       this.selected[obj.privilage] = true;
-  //     });
-  //   } else {
-  //     this.selected = {};
-  //   }
-  //   this.myList.options.forEach((option) => {
-  //     option.selected = this.selected[option.value.name];
-  //   });
-  // }
-
-  ngOnInit() {
-    this.assignRoleForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6),
-      ]),
-    });
-  }
-
-  onSelectionChange(event: MatSelectionListChange) {
-    this.selectedOptions = event.source.selectedOptions.selected.map(
-      (option) => option.value
-    );
-  }
-
-  onLogin() {
-    if (!this.assignRoleForm.valid) {
-      return;
-    }
-    console.log(this.assignRoleForm.value);
-    return {
-      email: this.assignRoleForm.value.email,
-      password: this.assignRoleForm.value.password,
-      privilages: this.selectedOptions,
-    };
-  }
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 }
