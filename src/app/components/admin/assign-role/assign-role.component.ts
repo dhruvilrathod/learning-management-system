9b811dbda48d6e0fdc9e994e57c2e0ce53f7c55f
John Doe <jghevariya@asite.com>
2023-03-29 15:50:06 +0530

    Added: Role Assignee Table

--- src/app/components/admin/assign-role/assign-role.component.ts
+++ src/app/components/admin/assign-role/assign-role.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
-const ELEMENT_DATA = [
-  { index: 1, name: 'Hydrogen', role: 1.0079, Edit: 'H' },
-  { index: 2, name: 'Helium', role: 4.0026, Edit: 'He' },
-  { index: 3, name: 'Lithium', role: 6.941, Edit: 'Li' },
-  { index: 4, name: 'Beryllium', role: 9.0122, Edit: 'Be' },
-  { index: 5, name: 'Boron', role: 10.811, Edit: 'B' },
-  { index: 6, name: 'Carbon', role: 12.0107, Edit: 'C' },
-  { index: 7, name: 'Nitrogen', role: 14.0067, Edit: 'N' },
-  { index: 8, name: 'Oxygen', role: 15.9994, Edit: 'O' },
-  { index: 9, name: 'Fluorine', role: 18.9984, Edit: 'F' },
-  { index: 10, name: 'Neon', role: 20.1797, Edit: 'Ne' },
+const assignees = [
+  { index: 1, name: 'Hydrogen', role: 1.0079, edit: 'H' },
+  { index: 2, name: 'Helium', role: 4.0026, edit: 'He' },
+  { index: 3, name: 'Lithium', role: 6.941, edit: 'Li' },
+  { index: 4, name: 'Beryllium', role: 9.0122, edit: 'Be' },
+  { index: 5, name: 'Boron', role: 10.811, edit: 'B' },
+  { index: 6, name: 'Carbon', role: 12.0107, edit: 'C' },
+  { index: 7, name: 'Nitrogen', role: 14.0067, edit: 'N' },
+  { index: 8, name: 'Oxygen', role: 15.9994, edit: 'O' },
+  { index: 9, name: 'Fluorine', role: 18.9984, edit: 'F' },
+  { index: 10, name: 'Neon', role: 20.1797, edit: 'Ne' },
 ];
 @Component({
   selector: 'app-assign-role',
@@ -18,8 +18,7 @@ const ELEMENT_DATA = [
   styleUrls: ['./assign-role.component.css'],
 })
 export class AssignRoleComponent implements OnInit {
+  dataSource = assignees;
+  displayedColumns: string[] = ['index', 'name', 'role', 'edit'];
   ngOnInit() {}
-
-  displayedColumns: string[] = ['position', 'name', 'role', 'Edit'];
-  dataSource = ELEMENT_DATA;
 }
--- src/app/models/interfaces.ts
+++ src/app/models/interfaces.ts
@@ -1,80 +1,86 @@
 export interface Talent {
-    personalDetails: {
-        name: string,
-        id?: string,
-        gender: string,
-        countryName: string,
-        age: string,
-    }
-    professionalDetails: {
-        currentProfessionalStatus: string;
-        industryType: string,
-        yearOfExperience: string,
-        skills: any[],
-        currentEmployerName: string,
-        linkedInProfileLink: string,
-        recordedVideoLink: string,
-        experienceInIT: string,
-        experienceLevel: number
-    }
-    educationDetails: {
-        nameOfDegree: string,
-        nameOfUniversity: string,
-        yearOfPassing: number
-    }[],
-    knownLanguages: string[],
-    fieldSpecialization: string[],
-    skillSpecialization: string,
-    profileImage: string,
-    email: string,
-    password: string,
+  personalDetails: {
+    name: string;
+    id?: string;
+    gender: string;
+    countryName: string;
+    age: string;
+  };
+  professionalDetails: {
+    currentProfessionalStatus: string;
+    industryType: string;
+    yearOfExperience: string;
+    skills: any[];
+    currentEmployerName: string;
+    linkedInProfileLink: string;
+    recordedVideoLink: string;
+    experienceInIT: string;
+    experienceLevel: number;
+  };
+  educationDetails: {
+    nameOfDegree: string;
+    nameOfUniversity: string;
+    yearOfPassing: number;
+  }[];
+  knownLanguages: string[];
+  fieldSpecialization: string[];
+  skillSpecialization: string;
+  profileImage: string;
+  email: string;
+  password: string;
 }
 
 export interface Trainer {
-    name: string;
-    id: string;
-    programs: any[],
-    yearOfExperience: string,
-    email: string,
-    profileImage: string
+  name: string;
+  id: string;
+  programs: any[];
+  yearOfExperience: string;
+  email: string;
+  profileImage: string;
 }
 
 export interface Grades {
-    talentName: string,
-    programName: string,
-    talentID?: string,
-    programID?: string,
-    overallGrades: string,
-    gradeDetails: {
-        mainSkillName: string,
-        mainSkillID: string,
-        subSkills: {
-            subSkillID: string,
-            subSkillName: string,
-            subSkillScore: string
-        }[]
-    }[]
+  talentName: string;
+  programName: string;
+  talentID?: string;
+  programID?: string;
+  overallGrades: string;
+  gradeDetails: {
+    mainSkillName: string;
+    mainSkillID: string;
+    subSkills: {
+      subSkillID: string;
+      subSkillName: string;
+      subSkillScore: string;
+    }[];
+  }[];
 }
 
 export interface Program {
-    id: string,
-    name: string,
-    trainers: string[],
-    thumbnailImageURL: string,
-    skillsList: any[],
-    programTracking: {
-        isPublished: boolean,
-        isStarted: boolean,
-        isEnded: boolean,
-        startDate?: string,
-        endDate?: string
-    }
-    duration?: string,
-    image?: string,
-    programOverview?: string,
-    totalEnrolledStudent?: number,
-    programOutcomes?: string,
-    overallGrades: string,
-    isEnrolled?: boolean,
-    grades?: Grades
+  id: string;
+  name: string;
+  trainers: string[];
+  thumbnailImageURL: string;
+  skillsList: any[];
+  programTracking: {
+    isPublished: boolean;
+    isStarted: boolean;
+    isEnded: boolean;
+    startDate?: string;
+    endDate?: string;
+  };
+  duration?: string;
+  image?: string;
+  programOverview?: string;
+  totalEnrolledStudent?: number;
+  programOutcomes?: string;
+  overallGrades: string;
+  isEnrolled?: boolean;
+  grades?: Grades;
+}
+
+export interface Assignees {
+  id: string;
+  name: string;
+  role: string;
 }
